7b38ae26aacdaa734df128cf6de52616f28a248d
Elliott Minns <elliott.minns@me.com>
2016-02-24 00:18:19 +0000

    Removed crud

---
+++ Xcode/Genetics/Genetics/Genetics.h
@@ -0,0 +1,19 @@
+//
+//  Genetics.h
+//  Genetics
+//
+//  Created by Elliott Minns on 23/02/2016.
+//  Copyright © 2016 Elliott Minns. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Genetics.
+FOUNDATION_EXPORT double GeneticsVersionNumber;
+
+//! Project version string for Genetics.
+FOUNDATION_EXPORT const unsigned char GeneticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Genetics/PublicHeader.h>
+
+
